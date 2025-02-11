8bdf2a104ab2bb2527ab615f108cdbf7caa459ad
Guilherme Abade <abade.guim13@gmail.com>
2025-02-10 22:42:21 -0300

    Operadores logicos em Python

---
+++ operadores.py
@@ -0,0 +1,13 @@
+""" idade = 21
+possui_convite = False
+filho_do_dono = True
+print((idade >= 21) and (possui_convite == True))
+print((idade >= 21) or (possui_convite == True))
+print((idade > 21) and (possui_convite == True) or (filho_do_dono == True))
+ """
+maior_de_idade = True
+possui_ctps = True
+esta_trabalhado_atualmente = True
+possui_veiculo_proprio = False
+print((maior_de_idade == True) and (possui_ctps == True))
+print(possui_ctps == True and not possui_veiculo_proprio)
