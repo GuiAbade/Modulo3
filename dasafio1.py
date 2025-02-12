0e0912dcaa349bdc89b56ddefbee7cded3c03848
Guilherme Abade <abade.guim13@gmail.com>
2025-02-11 21:32:14 -0300

    Usando operadores de igualdade

---
+++ dasafio1.py
@@ -0,0 +1,12 @@
+possui_passaporte = False
+passagem_comprada = False
+menor_de_idade = False
+
+# Uma pessoa só pode viajar se possuir  passaporte e tiver a passagem comprada e não for menor de idade
+print((possui_passaporte and passagem_comprada) and not menor_de_idade)
+# Uma pessoa só pode viajar se possuir passaporte ou tiver a passagem comprada e não for menor de idade
+print((possui_passaporte or passagem_comprada) and not menor_de_idade)
+# Uma pessoa só pode viajar se não possuir passaporte ou tiver a passagem comprada e não for menor de idade
+print((not possui_passaporte or passagem_comprada) and not menor_de_idade)
+# Uma pessoa não pode viajar se não possuir passaporte ou não tiver a passagem comprada e for menor de idade
+print((not possui_passaporte or not passagem_comprada) and menor_de_idade)
---
+++ operadoresDeIgualdade.py
@@ -0,0 +1,14 @@
+""" a = 'Python'
+b = 'Python'
+print(a == b)
+print(a is b) """
+# == compara valor
+# is compara a identidade
+""" 
+c = 10
+d = 10
+print(c == d)
+print(c is d) """
+
+cor_preferida = None
+print(cor_preferida is None)
