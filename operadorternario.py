b3cac3bc59362f7d339446469350ce7a71300479
Guilherme Abade <abade.guim13@gmail.com>
2025-02-12 20:27:42 -0300

    If ternário

---
+++ operadorternario.py
@@ -0,0 +1,18 @@
+# Caso a idade seja maior ou igual a 18 anos,
+# essa  pessoa é maior de idade, casi contrário, é menor de idade
+
+idade = 15
+
+print('Maior de idade') if idade >= 18 else print('Menor de idade')
+
+print('-----------------------------')
+
+possui_passaport = False
+
+print('Embarcar') if possui_passaport else print('Deve tirar passaporte')
+
+print('-----------------------------')
+
+velocidade = 80
+
+print('Você foi multado!') if velocidade >= 100 else print('Siga e frente')
