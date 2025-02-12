75a5f23b4b55cde432dc6e15f57e0bc930966e89
Guilherme Abade <abade.guim13@gmail.com>
2025-02-12 20:35:18 -0300

    Usando laço de repetição

---
+++ loop_for.py
@@ -0,0 +1,17 @@
+for numero in range(1, 10, 2):
+    print('carregando', numero)
+
+nomes = ['nome1', 'nome2', 'nome3', 'nome4']
+
+for nome in nomes:
+    print(nome)
+
+print('-----------------------------')
+
+# Desafio
+for i in range(18, 111):
+    print(i)
+
+# Desafio 2
+for i in range(1, 11):
+    print(f'Executando passo {i}')
